bddb32a9df48a2df03b2dbeeb75fc5bdd9658c1d
Oleg Tsarenkoff <tsarenkow@icloud.com>
2021-08-01 12:49:52 +0300

    Initial Commit

---
+++ ColorMatching_v2/ColorMatching_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  ColorMatching_v2App.swift
+//  ColorMatching_v2
+//
+//  Created by Oleg Tsarenkoff on 1.08.21.
+//
+
+import SwiftUI
+
+@main
+struct ColorMatching_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
